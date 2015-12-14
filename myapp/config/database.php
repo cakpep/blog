af6d3ff9401c3cdcfe98d31d2d1b2d8a589789b8
Febri Maschut Wicaksono <febri.maschut@redawning.com>
2015-12-14 18:44:10 +0700

    test

--- myapp/config/database.php
+++ myapp/config/database.php
@@ -46,17 +46,17 @@ return [
 
 	'connections' => [
 
-		'sqlite' => [
-			'driver'   => 'sqlite',
-			'database' => storage_path().'/database.sqlite',
-			'prefix'   => '',
-		],
+		// 'sqlite' => [
+		// 	'driver'   => 'sqlite',
+		// 	'database' => storage_path().'/database.sqlite',
+		// 	'prefix'   => '',
+		// ],
 
 		'mysql' => [
 			'driver'    => 'mysql',
 			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'forge'),
-			'username'  => env('DB_USERNAME', 'forge'),
+			'database'  => env('DB_DATABASE', 'larablog'),
+			'username'  => env('DB_USERNAME', 'root'),
 			'password'  => env('DB_PASSWORD', ''),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
@@ -64,25 +64,25 @@ return [
 			'strict'    => false,
 		],
 
-		'pgsql' => [
-			'driver'   => 'pgsql',
-			'host'     => env('DB_HOST', 'localhost'),
-			'database' => env('DB_DATABASE', 'forge'),
-			'username' => env('DB_USERNAME', 'forge'),
-			'password' => env('DB_PASSWORD', ''),
-			'charset'  => 'utf8',
-			'prefix'   => '',
-			'schema'   => 'public',
-		],
+		// 'pgsql' => [
+		// 	'driver'   => 'pgsql',
+		// 	'host'     => env('DB_HOST', 'localhost'),
+		// 	'database' => env('DB_DATABASE', 'forge'),
+		// 	'username' => env('DB_USERNAME', 'forge'),
+		// 	'password' => env('DB_PASSWORD', ''),
+		// 	'charset'  => 'utf8',
+		// 	'prefix'   => '',
+		// 	'schema'   => 'public',
+		// ],
 
-		'sqlsrv' => [
-			'driver'   => 'sqlsrv',
-			'host'     => env('DB_HOST', 'localhost'),
-			'database' => env('DB_DATABASE', 'forge'),
-			'username' => env('DB_USERNAME', 'forge'),
-			'password' => env('DB_PASSWORD', ''),
-			'prefix'   => '',
-		],
+		// 'sqlsrv' => [
+		// 	'driver'   => 'sqlsrv',
+		// 	'host'     => env('DB_HOST', 'localhost'),
+		// 	'database' => env('DB_DATABASE', 'forge'),
+		// 	'username' => env('DB_USERNAME', 'forge'),
+		// 	'password' => env('DB_PASSWORD', ''),
+		// 	'prefix'   => '',
+		// ],
 
 	],
 
